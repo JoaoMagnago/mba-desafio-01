391d503e2a3523eac79b386c4ed71daf946281d1
joao magnago <joaomagnago.wm@gmail.com>
2024-08-04 16:26:51 -0300

    feat: create function to load history data

--- src/main.js
+++ src/main.js
@@ -15,7 +15,38 @@ async function fetchClientById(id) {
 
 const cardIdButton = document.getElementById("search-card");
 const cardIdInput = document.getElementsByTagName("input")[0];
+const history = document.getElementById("history")
 
+const loadHistory = (appointmentHistory) => {
+  const historyCountSpan = document.getElementById("history-count")
+
+  historyCountSpan.textContent = `${appointmentHistory.length} cortes`
+
+  history.innerHTML = ""
+
+  appointmentHistory.map((haircut) => {
+    const item = document.createElement("li")
+    const dateTimeDiv = document.createElement("div")
+    const date = document.createElement("span")
+    const time = document.createElement("span")
+
+    date.textContent = haircut.date
+    time.textContent = haircut.time
+
+    const checkIcon = document.createElement("img")
+    checkIcon.classList.add("check-icon")
+    checkIcon.setAttribute("src", "./src/assets/icons/SealCheckGreen.svg")
+    checkIcon.setAttribute("alt", "Ícone de confirmação do corte")
+
+    dateTimeDiv.append(date, time)
+
+    item.append(dateTimeDiv, checkIcon)
+
+    item.classList.add("history-item")
+
+    history.appendChild(item)
+  })
+}
 
 cardIdButton.onclick = async (event) => {
   event.preventDefault()
@@ -23,8 +54,12 @@ cardIdButton.onclick = async (event) => {
   try {
     const cardId = cardIdInput.value
     const cardData = await fetchClientById(cardId)
-    console.log(cardData)
+
+    const { appointmentHistory } = cardData
+
+    loadHistory(appointmentHistory)
   } catch (error) {
     console.log(error)
   }
 }
+
